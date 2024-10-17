d26621af72bc524b7eb2780fea13398e14f140c8
QKF6858 <ethblacub@alum.us.es>
2024-10-17 12:44:46 +0200

    v3

--- src/entrenos_test.py
+++ src/entrenos_test.py
@@ -7,7 +7,10 @@ def lee_entrenos_test(ruta):
     print("-" * 30)
     print("\nProbando la función lee_entrenos...")
     print("Imprimiendo los primeros 3 entrenos...\n")
-    for entreno in lista_entrenos[1:4]:
+    for entreno in lista_entrenos[:3]:
+        print(entreno, "\n")
+    print("Imprimiendo los últimos 3 entrenos...\n")
+    for entreno in lista_entrenos[-3:]:
         print(entreno, "\n")
     print("-" * 30)
 
@@ -15,11 +18,11 @@ def lee_entrenos_test(ruta):
 
 
 def tipos_entrenos_test(entrenos):
-    tipos = tipos_entrenos(entrenos)
+    tipos = ", ".join(tipos_entrenos(entrenos))
 
     print("-" * 30)
     print("Probando la función tipos_entrenos...")
-    print(f"Los tipos de entrenos que hay en la lista son {tipos}")
+    print(f"Los tipos de entrenos que hay en la lista son: {tipos}.")
     print("-" * 30)
 
     
@@ -28,7 +31,9 @@ def entrenos_duracion_superior_test(lista_entrenos, d):
 
     print("-" * 30)
     print("Probando la función entrenos_duracion_superior...")
-    print(f"Los entrenos con duración superior a {d} son {entrenos_superiores}")
+    print(f"Los entrenos con duración superior a {d} son:\n")
+    for entreno in entrenos_superiores:
+        print(entreno, "\n")
     print("-" * 30)
 
 
@@ -45,7 +50,7 @@ def suma_calorias_test(lista_entrenos, f_inicio, f_fin):
 if __name__ == "__main__":
 
     lista_entrenos = lee_entrenos("data/entrenos.csv")
-    lee_entrenos_test("data/entrenos.csv")
+    # lee_entrenos_test("data/entrenos.csv")
     # tipos_entrenos_test(lista_entrenos)
-    # entrenos_duracion_superior_test(lista_entrenos, 115)
-    # suma_calorias_test(lista_entrenos, "5/5/2019", "10/10/2019")
+    # entrenos_duracion_superior_test(lista_entrenos, 125)
+    suma_calorias_test(lista_entrenos, "5/5/2019", "10/10/2019")
